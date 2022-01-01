a3df2a871467423eac8c96948778336dbc3003d4
Nifilat <latunde.nifemi2016@gmail.com>
2022-01-01 22:13:43 +0100

    Update Build Javascript Objects.js

---
+++ Build Javascript Objects.js
@@ -0,0 +1,9 @@
+const myDog = {
+    // Only change code below this line
+  "name": "Oluwanifemi",
+  "legs": 2,
+  "tails": 0,
+  "friends":["Tobi", "Dunsin", "YK"]
+  
+    // Only change code above this line
+  };
