a15af4cb7984e52d76f8e9cefdb998ea46ee18c8
Nifilat <latunde.nifemi2016@gmail.com>
2022-01-03 16:41:06 +0100

    Create Manipulating Complex Objects.js

---
+++ Manipulating Complex Objects.js
@@ -0,0 +1,23 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+    },
+    {
+      "artist": "Davido",
+      "title": "Assurance",
+      "release_year": 2019,
+      "formats": [
+        "CD",
+        "Youtube",
+        "Audio"
+      ]
+  
+    }
+  ];
