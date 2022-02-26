46e996bf151a0fcdf06102eb9b29e4744f034b0e
Nifilat <latunde.nifemi2016@gmail.com>
2022-02-26 23:13:10 +0100

    Create Explore Differences Between the var and let Keywords.js

---
+++ Explore Differences Between the var and let Keywords.js
@@ -0,0 +1,6 @@
+// Update the code so it only uses the let keyword.
+
+// unlike var, when you use let, a variable with the same name can only be declared once.
+
+let catName = "Oliver";
+let catSound = "Meow!";
