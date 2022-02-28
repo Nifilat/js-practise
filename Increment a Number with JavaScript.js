831b4ce99a22bfda8d274ee5adddefac62d9d8be
Nifilat <latunde.nifemi2016@gmail.com>
2022-02-28 06:53:48 +0100

    Create Increment a Number with JavaScript.js

---
+++ Increment a Number with JavaScript.js
@@ -0,0 +1,10 @@
+/*Change the code to use the ++ operator on myVar.
+let myVar = 87;
+
+// Only change code below this line
+myVar = myVar + 1; */
+
+let myVar = 87;
+
+// Only change code below this line
+myVar++;
