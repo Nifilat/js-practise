5d4788016bc3ac638bafabe248bf1e337c597521
Nifilat <latunde.nifemi2016@gmail.com>
2022-03-02 23:47:19 +0100

    Create Nest one Array within Another Array.js

---
+++ Nest one Array within Another Array.js
@@ -0,0 +1,4 @@
+// Create a nested array(This is also called a multi-dimensional array) called myArray. 
+
+// Only change code below this line
+const myArray = [["Oluwanifemi", 23], ["Oluwatobiloba", 22], ["Sugar cubes", 100]];
