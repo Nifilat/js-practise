c6b4a440417525ae1f2db5ea724811f4c137a6df
Nifilat <latunde.nifemi2016@gmail.com>
2022-03-14 17:16:00 +0100

    New folder

---
+++ ES6/Prevent Object Mutation.js
@@ -0,0 +1,20 @@
+function freezeObj() {
+  const MATH_CONSTANTS = {
+    PI: 3.14
+  };
+  // Only change code below this line
+  // This prevents the modofication of existing property and values and prevents addition of new properties.
+
+
+  Object.freeze(MATH_CONSTANTS);
+
+
+  // Only change code above this line
+  try {
+    MATH_CONSTANTS.PI = 99;
+  } catch(ex) {
+    console.log(ex);
+  }
+  return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
